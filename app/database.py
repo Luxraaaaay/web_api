a7a1076a5b1dc2f36c07cf099736b5e597d50b7d
Luxraaaaay <751318017@qq.com>
2026-04-11 22:45:59 +0800

    Configue database

---
+++ app/database.py
@@ -0,0 +1,18 @@
+import os
+from typing import Generator
+from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
+
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+engine = create_engine(DATABASE_URL, echo=True, connect_args=connect_args)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session() -> Generator[Session, None, None]:
+    with SessionLocal() as session:
+        yield session
